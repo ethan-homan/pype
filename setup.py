1d3f420f7f8f2630625628d322101a2b94f65fbc
Ethan <ethanwhoman@gmail.com>
2019-11-14 21:39:20 -0500

    add long description to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
 setup(
     name="pype",
     description="Run python one-liners on data in your terminal.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     version="0.0.1",
     author="Ethan Homan",
     author_email="ethanwhoman@gmail.com",
