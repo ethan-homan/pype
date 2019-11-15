49eed3031ad4bffec1b3aea05d844c320a87f73d
Ethan <ethanwhoman@gmail.com>
2019-11-14 21:28:05 -0500

    add to setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,15 @@ from setuptools import setup
 
 setup(
     name="pype",
+    description="Run python one-liners on data in your terminal.",
     version="0.0.1",
     author="Ethan Homan",
     author_email="ethanwhoman@gmail.com",
+    url="https://github.com/ethanwh/pype",
     license="Apache",
     scripts=['pype'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
 )
