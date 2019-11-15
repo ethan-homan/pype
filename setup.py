8c203c4dd3c5875cb10a3b87d715a871d6591793
Ethan <ethanwhoman@gmail.com>
2019-11-14 20:43:33 -0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from setuptools import setup
 setup(
     name="pype",
     version="0.0.1",
-    packages=["pype"],
     author="Ethan Homan",
     author_email="ethanwhoman@gmail.com",
     license="Apache",
-    entry_points={"console_scripts": ["pype=pype:main"]},
+    scripts=['pype'],
 )
